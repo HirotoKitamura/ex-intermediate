72d85bc0dd460628cb90fc180c79df97f36122b3
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-06-22 16:16:23 +0900

    課題2　ホテルのサービスクラスを作成

--- src/main/java/com/example/demo/service/Ex02Service.java
+++ src/main/java/com/example/demo/service/Ex02Service.java
@@ -1,5 +1,42 @@
 package com.example.demo.service;
 
-public class Ex02Service {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.Hotel;
+import com.example.demo.repository.Ex02Repository;
+
+/**
+ * 演習2のサービスクラス.
+ * 
+ * @author hiroto.kitamura
+ *
+ */
+@Service
+@Transactional
+public class Ex02Service {
+	@Autowired
+	private Ex02Repository repository;
+
+	/**
+	 * 指定の価格以下のホテルを価格の降順で検索.
+	 * 
+	 * @param price 指定価格
+	 * @return 検索結果
+	 */
+	public List<Hotel> searchByLessThanPrice(Integer price) {
+		return repository.searchByLessThanPrice(price);
+	}
+
+	/**
+	 * 全ホテルを価格の降順で検索.
+	 * 
+	 * @return 検索結果
+	 */
+	public List<Hotel> showAll() {
+		return repository.findAll();
+	}
 }
